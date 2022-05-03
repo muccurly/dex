66d9d19b827da155868ebd0ffc0135e20a735116
miras <mirmiras04@mail.ru>
2022-05-03 17:40:04 +0600

    added create pool with factory

--- packages/hardhat/contracts/Dex.sol
+++ packages/hardhat/contracts/Dex.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./Math.sol";
+import "./interfaces/IDexFactory.sol";
 contract Dex is ERC20 {
     using SafeMath for uint256;
 
-    // address public factoryAddress;
+    IDexFactory public factory;
 
     IERC20 public token1;
     IERC20 public token2;
@@ -24,42 +25,57 @@ contract Dex is ERC20 {
         address _token1,
         address _token2
     ) ERC20("LP-Token", "LPT") {
+        require(_token1 != address(0) || _token2 != address(0), 'Invalid token address');
         token1 = IERC20(_token1);
         token2 = IERC20(_token2);
-        // factoryAddress = msg.sender;
+        factory = IDexFactory(msg.sender);
     }
 
     function createPool(
         uint256 _amount1,
         uint256 _amount2
     ) external payable returns (uint256 amountLPTokens) {
-        uint256 balance1 = token1.balanceOf(address(this));
-        uint256 balance2 = token2.balanceOf(address(this));
+        (address token1_, address token2_) = _getTokenAdresses();
+        if(factory.getPools(token1_, token2_) == address(0)){
+            factory.createDex(token1_, token2_);
+        }
 
+        (uint256 balance1, uint256 balance2) = _getReserves();
         uint256 _totalSupply = totalSupply();
 
         if(_totalSupply == 0){
             amountLPTokens = Math.sqrt(_amount1.mul(_amount2));
         } else {
             amountLPTokens = Math.min(
-                (_amount1 * _totalSupply)/ balance1,
-                (_amount2 * _totalSupply)/ balance2);
+                    _amount1.mul(_totalSupply).div(balance1),
+                    _amount2.mul(_totalSupply).div(balance2)
+                );
         }
+        require(amountLPTokens <= 0, 'Insufficiently balance');
         token1.transferFrom(msg.sender, address(this), _amount1);
         token2.transferFrom(msg.sender, address(this), _amount2);
         _mint(msg.sender, amountLPTokens);
     }
 
 
-    function withdraw() external returns (address) {
+    function withdraw(uint256 _amount) external returns (address) {
 
     } 
 
     
     function swap(
-        uint _amount
+        uint _amount,
+        address _to
     ) external payable lock{
        
+    }
+
+    function _getTokenAdresses() internal view  returns (address tokenA, address tokenB) {
+        (tokenA, tokenB) = (address(token1), address(token2));
+    }
+
+    function _getReserves() internal view  returns (uint256 reserveA, uint256 reserveB) {
+        (reserveA, reserveB) = (token1.balanceOf(address(this)), token1.balanceOf(address(this)));
     }
 
     
--- packages/hardhat/contracts/DexFactory.sol
+++ packages/hardhat/contracts/DexFactory.sol
@@ -16,14 +16,23 @@ contract DexFactory {
         address _tokenA,
         address _tokenB
     ) public returns (address exchangeAddress) {
+        require(_tokenA == _tokenB, "Identical Addresses");
         require(_tokenA != address(0) || _tokenB != address(0), "Invalid token address");
         require(getPools[_tokenA][_tokenB] != address(0), "Dex of this tokens already exist");
 
-        
+        /// Create 1
         Dex exchange = new Dex(_tokenA, _tokenB);
+
         exchangeAddress = address(exchange);
         allPools.push(exchangeAddress);
         getPools[_tokenA][_tokenB] = exchangeAddress;
         getPools[_tokenB][_tokenA] = exchangeAddress;
     }
 }
+
+        /// Create 2 with Instructions
+        // bytes memory bytecode = type(Dex).creationCode;
+        // bytes32 salt = keccak256(abi.encodePacked(_tokenA, _tokenB));
+        // assembly {
+        //     pair := create(0, add(bytecode,32), mload(bytecode), salt)
+        // }
