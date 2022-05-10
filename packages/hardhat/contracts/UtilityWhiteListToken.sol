191d6ee6f1bb55e39906fe692c7abaf3431dd229
miras <mirmiras04@mail.ru>
2022-05-10 18:08:12 +0600

    added test for whiteList token

--- packages/hardhat/contracts/UtilityWhiteListToken.sol
+++ packages/hardhat/contracts/UtilityWhiteListToken.sol
@@ -3,28 +3,39 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+import "hardhat/console.sol";
 
 contract UtilityWhiteListToken is AccessControl, ERC20 {
     bytes32 public constant WHITELIST_ROLE = keccak256("WHITELIST_ROLE");
     constructor(
         string memory name,
         string memory symbol,
+        uint256 initialValue,     
         address[] memory whiteList
         )  ERC20(name, symbol){
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(WHITELIST_ROLE, _msgSender());
-
+        
         for (uint i = 0; i < whiteList.length; i++) {
-            revokeRole(WHITELIST_ROLE, whiteList[i]);
+            grantRole(WHITELIST_ROLE, whiteList[i]);
         }
+
+        _mint(_msgSender(), initialValue);
     }
 
+    function addToWhiteList(address account) external {
+        grantRole(WHITELIST_ROLE, account);
+    }
+
+    function removeFromWhiteList(address account) external {
+        revokeRole(WHITELIST_ROLE, account);
+    }
      function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal virtual override {
-        require(hasRole(WHITELIST_ROLE, from), 'Does not have access');
+        require(hasRole(WHITELIST_ROLE, to), 'Does not exist receiver address');
         super._beforeTokenTransfer(from, to, amount);
     }
 }
---
+++ packages/hardhat/test/UtilityWhiteListToken.test.js
@@ -0,0 +1,43 @@
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+
+use(solidity);
+
+describe("WhiteList Token", () => {
+    let owner, addr1, addr2;
+    let UtilityWhiteListToken;
+    let whiteListToken;
+
+    beforeEach(async () => {
+        [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+        UtilityWhiteListToken = await ethers.getContractFactory("UtilityWhiteListToken");
+        whiteListToken = await UtilityWhiteListToken.deploy("Test-Token", "TT", 1000, [addr1.address, addr2.address]);
+        await whiteListToken.deployed();
+    });
+    it("Transfer to WhiteList address", async () => {
+        await whiteListToken.transfer(addr1.address, 50);
+        expect(await whiteListToken.balanceOf(addr1.address)).to.equal(50);
+    });
+
+    it("Does not exist receiver address", async () => {
+        await expect(whiteListToken.transfer(addr3.address, 50)).to.be.revertedWith("Does not exist receiver address");
+        expect(await whiteListToken.balanceOf(addr3.address)).to.equal(0);
+    });
+
+    it("Should add an Address to WhiteList",async () => {
+        await whiteListToken.addToWhiteList(addr4.address);
+        await whiteListToken.transfer(addr4.address, 50);
+
+        expect(await whiteListToken.balanceOf(addr4.address)).to.equal(50);
+    });
+
+    it("Should remove an Address from WhiteList",async () => {
+        await whiteListToken.addToWhiteList(addr4.address);
+        await whiteListToken.transfer(addr4.address, 50);
+        expect(await whiteListToken.balanceOf(addr4.address)).to.equal(50);
+
+        await whiteListToken.removeFromWhiteList(addr4.address);
+        await expect(whiteListToken.transfer(addr4.address, 50)).to.be.revertedWith("Does not exist receiver address");
+    });
+})
